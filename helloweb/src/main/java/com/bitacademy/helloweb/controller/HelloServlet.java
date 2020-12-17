4ec89a75f74e485f8c4bd86f2eafdc66a5c4282f
javafullstack-bitacademy <javafullstack.bitacademy@gmail.com>
2020-12-17 15:53:31 +0900

    servlet & jsp 연습

---
+++ helloweb/src/main/java/com/bitacademy/helloweb/controller/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.bitacademy.helloweb.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("HelloServlet:doGet() called");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
