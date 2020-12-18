9f480b0b49933a6f0a44caab31689570f7490178
javafullstack-bitacademy <javafullstack.bitacademy@gmail.com>
2020-12-18 10:49:21 +0900

    servlet & jsp 연습#3

---
+++ helloweb/src/main/java/com/bitacademy/helloweb/controller/JoinServlet.java
@@ -0,0 +1,27 @@
+package com.bitacademy.helloweb.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JoinServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		System.out.println(email);
+		System.out.println(password);
+		
+		response.setContentType("text/html; charset=utf-8");
+		response.getWriter().println("ok!");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
