df1e7efc2ae473f21cb2f11b7e99718907e3c83c
javafullstack-bitacademy <javafullstack.bitacademy@gmail.com>
2020-12-24 12:47:44 +0900

    jstlel 연습#1

---
+++ el/src/main/java/jstlel/_01Servlet.java
@@ -0,0 +1,42 @@
+package jstlel;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class _01Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 값
+		int iVal = 10;
+		long lVal = 10;
+		float fVal = 1.4f;
+		boolean bVal = true;
+		String sVal = "가나다라마바사";
+		
+		
+		// 객체 테스트
+		Object obj = null;
+		UserVo vo = new UserVo();
+		vo.setNo(10L);
+		vo.setName("안대혁");
+		
+		
+		request.setAttribute("iVal", iVal);
+		request.setAttribute("lVal", lVal);
+		request.setAttribute("fVal", fVal);
+		request.setAttribute("bVal", bVal);
+		request.setAttribute("sVal", sVal);
+		request.setAttribute("obj", obj);
+		request.setAttribute("vo", vo);
+		request.getRequestDispatcher("/WEB-INF/views/01.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
